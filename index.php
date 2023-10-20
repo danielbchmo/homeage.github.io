cfd95ee2a1f5f734379d19fde6941084a1ca1119
danielbchmo <danielbchmo123@gmail.com>
2023-10-20 13:58:01 -0600

    Agreando archivos al sitio

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="HomeAge">
+    <meta name="description" content="Plataforma web de la organizacion HomeAge">
+    <link rel="stylesheet" href="css/lux.css">
+    <title>Sociedad</title>
+
+    <style>
+    .img-logo {
+        background-color: #000000;
+        text-align: center;
+    }
+
+    .img-logo-footer {
+        text-align: center;
+    }
+
+    #footer-contact {
+        text-align: center;
+    }
+    </style>
+
+</head>
+
+<body>
+    <!--///////////////////////////////////////////////////// HEADER-->
+    <header>
+        <!--////   Logo    ////-->
+        <div class="img-logo">
+            <img src="img/homeAgeNegro.png" alt="logo" width="200px" height="200px">
+        </div>
+        <!--////   Nav    ////-->
+        <nav>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Servicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Nosotros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <!--/////////////////////////////////////////////////// MAIN-->
+    <main>
+        <!--////   Centro    ////-->
+        <div class="container text-center">
+            <div class="row">
+                <!--////   Izquierda: Card    ////-->
+                <div class="col">
+                    <div class="card mb-3">
+                        <h3 class="card-header">Servicio</h3>
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <h6 class="card-subtitle text-muted">Support card subtitle</h6>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%"
+                            height="200" aria-label="Placeholder: Image cap" focusable="false" role="img"
+                            preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180"
+                            style="font-size:1.125rem;text-anchor:middle">
+                            <rect width="100%" height="100%" fill="#868e96"></rect>
+                            <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+                        </svg>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <!--////   Centro: Card    ////-->
+                <div class="col">
+                    <div class="card mb-3">
+                        <h3 class="card-header">Nosotros</h3>
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <h6 class="card-subtitle text-muted">Support card subtitle</h6>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%"
+                            height="200" aria-label="Placeholder: Image cap" focusable="false" role="img"
+                            preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180"
+                            style="font-size:1.125rem;text-anchor:middle">
+                            <rect width="100%" height="100%" fill="#868e96"></rect>
+                            <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+                        </svg>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <!--////   Derecha: Card    ////-->
+                <div class="col">
+                    <div class="card mb-3">
+                        <h3 class="card-header">Contacto</h3>
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <h6 class="card-subtitle text-muted">Support card subtitle</h6>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%"
+                            height="200" aria-label="Placeholder: Image cap" focusable="false" role="img"
+                            preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180"
+                            style="font-size:1.125rem;text-anchor:middle">
+                            <rect width="100%" height="100%" fill="#868e96"></rect>
+                            <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+                        </svg>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!--/////////////////////////////////////////////////// FOOTER-->
+    <footer>
+        <div class="list-group">
+            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">HomeAge</h5>
+                    <small>Regresar arriba</small>
+                </div>
+                <p class="mb-1">Una forma de brindar seguridad.</p>
+            </a>
+            <div id="footer-contact">
+                <small class="list-group-item list-group-item-action flex-column align-items-start active">
+                    <img class="img-whatsapp" src="img/whatsapp.png" alt="whatsapp contacto" width="20px">
+                    +52 33 2236 4326
+                    <br>
+                    <a href="https://www.facebook.com/oscarin.bchmo.37/" target="_blank"
+                        class="list-group-item active">
+                        <img class="img-facebook" src="img/facebook.png" alt="pagina de facebook" width="20px">
+                        HomeAge
+                    </a>
+                    <div class="img-logo-footer">
+                        <img src="img/homeAgeNegroBlanco2.png" alt="logo en el footer" width=100px>
+                    </div>
+                </small>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
